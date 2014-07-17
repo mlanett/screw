7c77dcde2896f59ea3233e432ee9e41438ac90d2
Mark Lanett <mark.lanett@gmail.com>
2014-07-17 00:38:00 -0700

    Cleanup.

--- lib/screw.rb
+++ lib/screw.rb
@@ -1,9 +1,9 @@
 require "screw/version"
 
 module Screw
-  autoload :Logger, "screw/logger"
-  autoload :Proxy,  "screw/proxy"
-  autoload :Queue,  "screw/queue"
+  autoload :Logger,     "screw/logger"
+  autoload :Proxy,      "screw/proxy"
+  autoload :Queue,      "screw/queue"
   autoload :Semaphore,  "screw/semaphore"
 
   extend Logger
